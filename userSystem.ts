468097f4dc7e46754006dea49421776842aae385
Nico <141285419+red4cted25@users.noreply.github.com>
2025-04-10 14:11:32 -0700

    Created, Completed UserSystem.ts
    Created first version of UserSystem using enums for Role and Status, creating an interface for a User object, and functions for whether or not a user can access the admin panel before finally suspending inactive users.

---
+++ userSystem.ts
@@ -0,0 +1,79 @@
+enum Role {
+    Admin = 'Admin',
+    Moderator = 'Moderator',
+    Member = 'Member',
+    Guest = 'Guest'
+}
+
+enum Status {
+    Active = 'Active',
+    Inactive = 'Inactive',
+    Suspended = 'Suspended',
+    Pending = 'Pending'
+}
+
+interface User {
+    id: number;
+    username: string;
+    email: string;
+    role: Role;
+    status: Status;
+}
+
+function canAccessAdminPanel(user: User): boolean {
+    return user.status === Status.Active ? (user.role === Role.Admin || user.role === Role.Moderator ? true : false) : false;
+}
+
+function suspendInactiveUsers(users: User[]): User[] {
+    return users.map(user => {
+        if (user.status === Status.Inactive) {
+            user.status = Status.Suspended;
+        }
+        return user;
+    });
+}
+
+const users: User[] = [
+    {
+        id: 1,
+        username: 'DanielDaGoat',
+        email: 'danielfree927@gmail.com',
+        role: Role.Admin,
+        status: Status.Active
+    },
+    {
+        id: 2,
+        username: 'DerpNerdd',
+        email: 'derpNerdd@yahoo.com',
+        role: Role.Moderator,
+        status: Status.Suspended
+    },
+    {
+        id: 3,
+        username: 'heyitsme.a',
+        email: 'audreygeeha439@example.com',
+        role: Role.Member,
+        status: Status.Pending
+    },
+    {
+        id: 4,
+        username: 'sabrinatheTeenageWitch',
+        email: 'salemRobot@msn.com',
+        role: Role.Guest,
+        status: Status.Inactive
+    },
+    {
+        id: 5,
+        username: 'NicoDaBest25',
+        email: 'nicolas.m.diaz07@gmail.com',
+        role: Role.Member,
+        status: Status.Active
+    }
+];
+
+for (const user of users) {
+    console.log(`${user.username} can access admin panel: ${canAccessAdminPanel(user)}`);
+}
+console.log('Suspending inactive users...');
+const updatedUsers = suspendInactiveUsers(users);
+console.log('Updated users:', updatedUsers);
